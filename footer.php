b867a707e454afb78e4313686cb10501e7259b33
Spouw <saj.pouw@gmail.com>
2018-08-27 14:06:30 +0200

    verandering in de footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying the footer.
+ * The template for displaying the footer. Met een verandering
  *
  * Contains the closing of the #content div and all content after
  *
